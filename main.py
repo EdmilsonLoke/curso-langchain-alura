cc63ac6afdabe885a7a35b05928edddece1f0e92
EdmilsonLoke <edmilsonfernandessneto@gmail.com>
2026-09-12 15:23:26 -0300

    Adiciona estrutura de PromptTemplate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 
@@ -7,18 +8,29 @@ api_key = os.getenv("GOOGLE_API_KEY")
 
 numero_dias = 7
 numero_criancas = 2
-atividade = "musica"
+atividade = "praia"
 
-prompt = f"Crie um roteiro de viagem de {numero_dias} dias, para uma familia com {numero_criancas} criancas, que gosta de {atividade}"
-
-cliente = ChatGoogleGenerativeAI(
-    model="gemini-3.6-flash",
-    google_api_key=api_key
+modelo_de_prompt = PromptTemplate(
+    template="""
+    Crie um roteiro de viagem de {dias}
+    para uma familia com {numero_criancas}
+    que gostam de {atividade}
+     """
 )
 
-resposta = cliente.invoke([
-    ("system", "Voce é um assistente de roteiros de viagens."),
-    ("human", prompt)
-])
+prompt = modelo_de_prompt.format(
+    dias = numero_dias,
+    numero_criancas = numero_criancas,
+    atividade = atividade
+)
 
+print("Prompt : \n", prompt)
+
+modelo = ChatGoogleGenerativeAI(
+    model="gemini-3.6-flash",
+    temperature=0.5,
+    api_key=api_key
+)
+
+resposta = modelo.invoke(prompt)
 print(resposta.content)
