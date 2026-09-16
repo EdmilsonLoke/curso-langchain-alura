e76527e384e80fc114958357b0c19b3210d57bce
EdmilsonLoke <edmilsonfernandessneto@gmail.com>
2026-09-16 15:40:31 -0300

    Adiciona saída estruturada com Pydantic e debug

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
+from langchain.globals import set_debug
 import os
+
+set_debug(True)
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 
+class Destino(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    motivo:str = Field("Motivo pelo qual é interessante visitar essa cidade")
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
 prompt_cidade = PromptTemplate(
     template="""
     Sugira uma cidade dado o meu interesse por {interesse}.
+    {formato_de_saida}
     """,
-    input_variables=["interesse"]
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
 )
 
 modelo = ChatGoogleGenerativeAI(
@@ -20,7 +32,7 @@ modelo = ChatGoogleGenerativeAI(
     api_key=api_key
 )
 
-cadeia = prompt_cidade | modelo | StrOutputParser()
+cadeia = prompt_cidade | modelo | parseador
 
 resposta = cadeia.invoke(
     {
